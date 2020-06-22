b55136505cc51f8b3f161bcf726dc3ba5b72c1d8
Muhammad Musa <moosasaadat99@gmail.com>
2020-06-22 12:18:22 +0500

    Initial Commit

---
+++ test_heredity.py
@@ -0,0 +1,11 @@
+import unittest
+import heredity as hrd
+
+
+class TestHeredity(unittest.TestCase):
+    def test_joint_probability(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
