f404e7b8acc222b13bee86f9023e71d9db1cd3aa
Muhammad Musa <moosasaadat99@gmail.com>
2020-06-22 14:19:53 +0500

    Calculate probability of person with no parents

--- heredity.py
+++ heredity.py
@@ -3,37 +3,18 @@ import itertools
 import sys
 
 PROBS = {
-
     # Unconditional probabilities for having gene
-    "gene": {
-        2: 0.01,
-        1: 0.03,
-        0: 0.96
-    },
-
+    "gene": {2: 0.01, 1: 0.03, 0: 0.96},
     "trait": {
-
         # Probability of trait given two copies of gene
-        2: {
-            True: 0.65,
-            False: 0.35
-        },
-
+        2: {True: 0.65, False: 0.35},
         # Probability of trait given one copy of gene
-        1: {
-            True: 0.56,
-            False: 0.44
-        },
-
+        1: {True: 0.56, False: 0.44},
         # Probability of trait given no gene
-        0: {
-            True: 0.01,
-            False: 0.99
-        }
+        0: {True: 0.01, False: 0.99},
     },
-
     # Mutation probability
-    "mutation": 0.01
+    "mutation": 0.01,
 }
 
 
@@ -46,17 +27,7 @@ def main():
 
     # Keep track of gene and trait probabilities for each person
     probabilities = {
-        person: {
-            "gene": {
-                2: 0,
-                1: 0,
-                0: 0
-            },
-            "trait": {
-                True: 0,
-                False: 0
-            }
-        }
+        person: {"gene": {2: 0, 1: 0, 0: 0}, "trait": {True: 0, False: 0}}
         for person in people
     }
 
@@ -66,8 +37,10 @@ def main():
 
         # Check if current set of people violates known information
         fails_evidence = any(
-            (people[person]["trait"] is not None and
-             people[person]["trait"] != (person in have_trait))
+            (
+                people[person]["trait"] is not None
+                and people[person]["trait"] != (person in have_trait)
+            )
             for person in names
         )
         if fails_evidence:
@@ -110,8 +83,13 @@ def load_data(filename):
                 "name": name,
                 "mother": row["mother"] or None,
                 "father": row["father"] or None,
-                "trait": (True if row["trait"] == "1" else
-                          False if row["trait"] == "0" else None)
+                "trait": (
+                    True
+                    if row["trait"] == "1"
+                    else False
+                    if row["trait"] == "0"
+                    else None
+                ),
             }
     return data
 
@@ -122,7 +100,8 @@ def powerset(s):
     """
     s = list(s)
     return [
-        set(s) for s in itertools.chain.from_iterable(
+        set(s)
+        for s in itertools.chain.from_iterable(
             itertools.combinations(s, r) for r in range(len(s) + 1)
         )
     ]
@@ -139,7 +118,54 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    raise NotImplementedError
+    one_gene = {"Harry"}
+    two_genes = {"James"}
+
+    def calcProb(person):
+        """Returns probability of person"""
+        mother = people[person]["mother"]
+        father = people[person]["father"]
+        # Has parents
+        if father and mother:
+            # Mother Info
+            motherGenes = 0
+            if mother in one_gene:
+                motherGenes = 1
+            elif mother in two_genes:
+                motherGenes = 2
+            motherHasTrait = True if mother in have_trait else False
+
+            # Father Info
+            fatherGenes = 0
+            if father in one_gene:
+                fatherGenes = 1
+            elif father in two_genes:
+                fatherGenes = 2
+            fatherHasTrait = True if father in have_trait else False
+
+            # Person Info
+            personGenes = 0
+            if person in one_gene:
+                personGenes = 1
+            elif person in two_genes:
+                personGenes = 2
+            personHasTrait = True if person in have_trait else False
+
+        # No parents
+        else:
+            # Genes in person
+            geneCount = 0
+            if person in one_gene:
+                geneCount = 1
+            elif person in two_genes:
+                geneCount = 2
+
+            # Trait of person
+            hasTrait = True if person in have_trait else False
+
+            # Calc prob
+            final_prob = PROBS["gene"][geneCount] * PROBS["trait"][geneCount][hasTrait]
+            return final_prob
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
