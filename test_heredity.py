9bc60dc4f6278bee0528df7ee84af0cb53973c5a
Muhammad Musa <moosasaadat99@gmail.com>
2020-06-22 19:51:28 +0500

    Add test case for update()

--- test_heredity.py
+++ test_heredity.py
@@ -34,6 +34,27 @@ class TestHeredity(unittest.TestCase):
             5,
         )
 
+    def test_update(self):
+
+        probabilities = {
+            "Harry": {"gene": {2: 0, 1: 0, 0: 0}, "trait": {True: 0, False: 0}},
+            "James": {"gene": {2: 0, 1: 0, 0: 0}, "trait": {True: 0, False: 0}},
+            "Lilly": {"gene": {2: 0, 1: 0, 0: 0}, "trait": {True: 0, False: 0}},
+        }
+        one_gene = {"Harry"}
+        two_genes = {"James"}
+        have_trait = {"James"}
+        p = 0.00266
+        hrd.update(probabilities, one_gene, two_genes, have_trait, p)
+        self.assertEqual(
+            probabilities,
+            {
+                "Harry": {"gene": {2: 0, 1: p, 0: 0}, "trait": {True: 0, False: p}},
+                "James": {"gene": {2: p, 1: 0, 0: 0}, "trait": {True: p, False: 0}},
+                "Lilly": {"gene": {2: 0, 1: 0, 0: p}, "trait": {True: 0, False: p}},
+            },
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
